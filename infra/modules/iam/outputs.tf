a8e310e6262fc71bdd0fa715a0740b517dd6d335
Abid hussain <abidhussain01101@gmail.com>
2025-10-08 19:58:55 +0500

    5th commit

--- infra/modules/iam/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ecs_task_execution_role_arn" {
-  value = aws_iam_role.ecs_task_execution.arn
-}
-
-output "github_oidc_role_arn" {
-  value = length(var.github_oidc_provider_arn) > 0 ? aws_iam_role.github_actions_oidc[0].arn : ""
-}
